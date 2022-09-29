19948214328cbbfd508c04ef9cab6cce8e72055b
MARIA LESNIKOVA <113589553+cap1bar4@users.noreply.github.com>
2022-09-29 11:50:18 +0300

    Add files via upload

---
+++ Lesnikova_Maria_221_331_lab2.cpp
@@ -0,0 +1,85 @@
+﻿#include<iostream>
+
+
+using  std::endl;
+using  std::cout;
+using  std::cin;
+
+void task1()
+{
+	int i, var, j;
+
+	cout << "Enter num: ";
+	cin >> var;
+	for (i = 0; i <= var; i++)
+	{
+		for (j = 0; j <= i; j++)
+		{
+			cout << j;
+		}
+		cout << endl;
+	}
+	cout << endl;
+}
+void task2()
+{
+	int n;
+	double c = 1;
+	cout << "Enter: ";
+	cin >> n;
+	cout << c << " ";
+	for (int k = 0; n > k; k++)
+	{
+		cout << c * (n - k) / (k + 1) << " ";
+		c = c * (n - k) / (k + 1);
+	}
+	cout << endl;
+}
+void task3()
+{
+	cout << "Enter num:\n";
+	int summ = 0, c = 0;
+	int num, u = 0;
+	while (u == 0) {
+		cin >> num;
+		if (num == 0) {
+			u ++;
+			continue;
+		}
+		c++;
+		summ += num;
+	}
+	cout << "Answer: " << double(summ) / c << endl;
+}
+
+int main()
+{
+	int choice = 0;
+	while (true)
+	{
+		cout << "\t<<Menu>>\n1 - 'Number Triangle'\n2 - 'Binomial coefficient'\n3 - 'Calculation of the arithmetic mean'\n";
+		cin >> choice;
+		switch (choice)
+		{
+		case 1:
+		{
+			task1();
+			break;
+		}
+		case 2:
+		{
+			task2();
+			break;
+		}
+		case 3:
+		{
+			task3();
+			break;
+		}
+		default:
+			return 0;
+		}
+
+	}
+}
+
---
+++ Lesnikova_laba_1_221_331.cpp
@@ -0,0 +1,78 @@
+﻿#include<iostream>
+#include<bitset>
+#include<string>
+#include<limits.h>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::hex;
+using std::bitset;
+
+int main()
+{
+	const char* tab = "\t";
+	cout << tab << char(35) << "1\n";
+	cout << "Lesnikova Maria ilinichna, 221-331\n";
+
+	cout << tab << char(35) << "2\n";
+	cout << "int min: " << int(0b10000000000000000000000000000000) << " , int max: " << unsigned(0b11111111111111111111111111111111) << " , size of int: " << sizeof(int) << endl;
+
+	unsigned int first;
+
+	cout << "min unsigned int: -" << unsigned(0b0000000000000000) << " , max unsigned int: " << unsigned(0b1111111111111111) << " , size of unsigned int: " << sizeof(first) << endl;
+
+	cout << "min short: " << short(0b1000000000000000) << " , max short: " << short(0b0111111111111111) << " , size of short: " << sizeof(short) << endl;
+
+	unsigned short second;
+	cout << "min unsigned short: -" << " , max unsigned short: " << USHRT_MAX << " , size of short: " << sizeof(second) << endl;
+
+	cout << "min long: " << LONG_MIN << " , max long: " << LONG_MAX << " , size of long: " << sizeof(long) << endl;
+
+	cout << "min long long: " << LLONG_MIN << " , max long long: " << LLONG_MAX << " , size of long long: " << sizeof(long long) << endl;
+
+	cout << "min double: - 308 (15 знаков) " << " , max double: 1,7E +" << " , size of double: " << sizeof(double) << endl;
+
+	cout << "min char: " << CHAR_MIN << ", max char: " << CHAR_MAX << " , size of char: " << sizeof(char) << endl;
+
+	cout << "min bool: false or 0 " << "max bool: true or 1" << " , size of bool: " << sizeof(bool) << endl;
+
+
+	cout << tab << char(35) << "3\n";
+	cout << "Enter an integer number: ";
+	int var;
+	cin >> var;
+	cout << "in binary form: " << bitset<8 * sizeof(var)>(var) << endl;
+	cout << "in hexadecimal : " << hex << var << endl;
+	cout << "bool: " << bool(var) << endl;
+	cout << "double: " << double(var) << endl;
+	cout << "char: " << char(var) << endl;
+
+
+	cout << tab << char(35) << "4\n";
+	cout << "Enter odds a * x = b" << endl;
+
+	cout << "a = ";
+		
+	int a, b, x;
+
+	cin >> a;
+
+	cout << "b = ";
+
+	cin >> b;
+	x = b / a;
+	cout << "x = " << b << "/" << a << endl;
+	cout << "x = " << x << endl;
+	cout << "answer: " << x << endl;
+
+	
+	cout << tab << char(35) << "5\n";
+	cout << "Enter the coordinates of the line segment: " << endl;
+	cout << "first point: ";
+	int var1, var2;
+	cin >> var1;
+	cout << "second point: ";
+	cin >> var2;
+	cout << "The middle of the segment is at the point with coordinate: " << (var1 + var2) / 2.;
+}
